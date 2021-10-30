007092f1e85366195dcc34ebb48531006e6de7b1
Veronika Hryharovich <weronnika@list.ru>
2021-10-30 15:19:34 +0300

    storing passwords in a database in unencrypted form

--- src/main/java/com/grigorovich/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/grigorovich/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.grigorovich.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,15 +8,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity //эта аннотация явялется конфигурацией можно configuration не писать
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder= User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("Veronika").password("Veronika").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("Elena").password("Elena").roles("HR"))
-                .withUser(userBuilder.username("Ivan").password("Ivan").roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override //делаем авторизацию
